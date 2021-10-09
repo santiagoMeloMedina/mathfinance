b1bd922c9c28dc4a479689f6d071f123f0a67acf
BuildTools <unconfigured@null.spigotmc.org>
2021-10-09 00:54:36 -0500

    mutually in progress

--- auto/src/metrics/bussiness.py
+++ auto/src/metrics/bussiness.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Any, List
 
 import numpy_financial as npf
 
@@ -7,10 +7,11 @@ from src.metrics import money_by_time as mbt
 
 
 class Project:
-    def __init__(self, amounts: List[float] = [], TCO: float = 0):
+    def __init__(self, amounts: List[float] = [], TCO: float = 0, id: Any = None):
         self.amounts = amounts
         self.TCO = models.Percentage(TCO)
         self.__set_VDT(amounts)
+        self.id = id
 
     def reset(self):
         self.amounts = []
@@ -53,3 +54,35 @@ class Project:
 
     def add_amount(self, value: float):
         self.amounts.append(value)
+
+
+class MutuallyEsclusive:
+    def __init__(self, projects: List[Project] = []):
+        self.projects = projects
+
+    def add_project(self, project: Project):
+        self.projects.append(project)
+
+    def __is_greater_viable(self, project_a: Project, project_b: Project) -> bool:
+        pass
+
+    @property
+    def _rank(self):
+        vps = sorted(self.projects, key=lambda x: x._VPN)
+        tirs = sorted(self.projects, key=lambda x: x._TIR)
+
+        self.ranking = []
+
+        vpn_i, tir_i = 0, 0
+        while vpn_i < len(vps) or tir_i < len(tirs):
+            if vpn_i == len(vps):
+                self.ranking.append(tirs[tir_i])
+                tir_i += 1
+            elif tir_i == len(tirs):
+                self.ranking.append(vps[vpn_i])
+                vpn_i += 1
+            else:
+                if vps[vpn_i].id == tirs[tir_i].id:
+                    self.ranking.append(vps[vpn_i])
+                    vpn_i += 1
+                    tir_i += 1
