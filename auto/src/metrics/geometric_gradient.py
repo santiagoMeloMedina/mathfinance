f833b487ceb1cee1ab7351acfa6783f7100e3609
BuildTools <unconfigured@null.spigotmc.org>
2021-10-10 00:07:03 -0500

    adjusted geometric gradient implementation and added tests

--- auto/src/metrics/geometric_gradient.py
+++ auto/src/metrics/geometric_gradient.py
@@ -1,14 +1,4 @@
-from src import terms, rates, models, util
-
-import json
-
-"""
-    VP: Total value
-    B: Base value
-    J: Geometric gradient
-    N: N term
-    Ip: Interest rates.Rate
-"""
+from src import models
 
 
 class GeometricGradient:
@@ -21,6 +11,13 @@ class GeometricGradient:
         VP: float = None,
         decimal: int = 3,
     ):
+        """
+        VP: Total value
+        B: Base value
+        J: Geometric gradient
+        N: N term
+        Ip: Interest rates.Rate
+        """
         self.B = B
         self.J = models.Percentage(J)
         self.N = N
@@ -35,31 +32,31 @@ class GeometricGradient:
         self.interests = dict()
         self.payments = dict()
 
-    def get_n_fee(self, n):
+    def _fee(self, n):
         if n in self.fees:
             result = self.fees[n]
         elif n == 1:
             result = self.B
         else:
-            result = self.get_n_fee(n - 1) * (1 + self.J.real)
+            result = self._fee(n - 1) * (1 + self.J.real)
         self.fees[n] = result
         return result
 
-    def get_n_interest(self, n):
-        self.interests[n] = self.get_n_balance(n - 1) * self.Ip.real
+    def _interest(self, n):
+        self.interests[n] = self._balance(n - 1) * self.Ip.real
         return self.interests[n]
 
-    def get_n_payment(self, n):
-        self.payments[n] = self.get_n_fee(n) - self.get_n_interest(n)
+    def _payment(self, n):
+        self.payments[n] = self._fee(n) - self._interest(n)
         return self.payments[n]
 
-    def get_n_balance(self, n):
+    def _balance(self, n):
         if n in self.balances:
             result = self.balances[n]
         elif n == 0:
             result = self.VP
         else:
-            result = self.get_n_balance(n - 1) - self.get_n_payment(n)
+            result = self._balance(n - 1) - self._payment(n)
         self.balances[n] = result
         return result
 
@@ -71,57 +68,44 @@ class GeometricGradient:
             for n in range(len(values)):
                 values[n] = round(values[n], self.decimal)
 
-    def get_totals(self):
+    @property
+    def _last_metrics(self):
         last_metrics = {
-            "interest": self.get_n_interest(self.N),
-            "payment": self.get_n_payment(self.N),
-            "fee": self.get_n_fee(self.N),
-            "balance": self.get_n_balance(self.N),
+            "interest": self._interest(self.N),
+            "payment": self._payment(self.N),
+            "fee": self._fee(self.N),
+            "balance": self._balance(self.N),
         }
+        self.round_values(last_metrics)
+        return last_metrics
+
+    @property
+    def get_totals(self):
+        self._last_metrics
         total = {
             "interest": sum(self.interests.values()),
             "payments": sum(self.payments.values()),
             "fees": sum(self.fees.values()),
         }
-        self.round_values(last_metrics)
         self.round_values(total)
-        return {
-            "last_metrics": last_metrics,
-            "total_metrics": total,
-        }
+        return total
 
     @property
     def _VP(self):
         p1 = 1 / (self.J - self.Ip)
         p2 = (((1 + self.J.real) / (1 + self.Ip.real)) ** self.N) - 1
         self.VP = self.B * p1 * p2
-        return util.format_money(self.VP, decimal=self.decimal)
+        return self.VP
 
     @property
     def _B(self):
         p1 = 1 / (self.J - self.Ip)
         p2 = (((1 + self.J.real) / (1 + self.Ip.real)) ** self.N) - 1
         self.B = self.VP / (p1 * p2)
-        return util.format_money(self.B, decimal=self.decimal)
+        return self.B
 
     @property
     def _B_w_VP_Ip_J(self):
         tmp = self.Ip - self.J
         self.B = self.VP * tmp
-        return util.format_money(self.B, decimal=self.decimal)
-
-
-# ip = rates.rates.RateOperation.simple(
-#     rates.Rate=rates.Rate(effective=4.20, nper=1, d_nper=12),
-#     source=rates.rates.RateOperation.EFFECTIVE,
-#     target=rates.rates.RateOperation.IPV,
-# )
-
-# term = terms.Terms.many(term=terms.Terms.MONTH, years=3)yt76hy8
-
-# gg = GeometricGradient(VP=200000000, J=0.20, N=360, Ip=ip["result"].value)
-# print("B", gg._B_w_VP_Ip_J)
-
-# print(util.format_money(gg.get_n_fee(360), 3))
-
-# print(json.dumps(gg.get_totals(), indent=2))
+        return self.B
